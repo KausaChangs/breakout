b5f0fdadae9fe551d787f760e6d73bd987e79a74
KausaChangs <changayakausa@gmail.com>
2023-03-28 09:10:55 +0200

    we've drawn our first block

--- app.js
+++ app.js
@@ -15,18 +15,16 @@ class Block {
 //all my blocks
 const blocks = [new Block(10, 270)];
 
-console.log(blocks[0]);
-
 //draw all my blocks
 
-// function addBlocks() {
-//   for (let i = 0; i < blocks.length; i++) {
-//     const block = document.createElement("div");
-//     block.classList.add("block");
-//     block.style.left = "100px";
-//     block.style.bottom = "50px";
-//     grid.appendChild(block);
-//   }
-// }
+function addBlocks() {
+  for (let i = 0; i < blocks.length; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.style.left = blocks[i].bottomLeft[0] + "px";
+    block.style.bottom = blocks[i].bottomLeft[1] + "px";
+    grid.appendChild(block);
+  }
+}
 
-// addBlocks();
+addBlocks();
