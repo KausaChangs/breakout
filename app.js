f876d75f3eb4d4f5b746e55be9fc9d1236049e76
KausaChangs <changayakausa@gmail.com>
2023-05-30 12:12:45 +0200

    my final commit

--- app.js
+++ app.js
@@ -131,6 +131,13 @@ function checkForCollisions() {
       changeDirection();
       score++;
       scoreDisplay.innerHTML = score;
+
+      //check for win
+      if (blocks.length === 0) {
+        scoreDisplay.innerHTML = "YOU WIN!";
+        clearInterval(timerId);
+        document.removeEventListener("keydown", moveUser);
+      }
     }
   }
 
